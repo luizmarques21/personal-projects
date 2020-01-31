57008609f7b260ee7ab32dc67991d4b0c7dddf45
Luiz Mariel Leite Marques <luizmariel@moobitech.com.br>
2020-01-31 11:58:30 -0300

    Exercicios finais do curso de JS Avançado 1

--- personal-projects/js-avancado1/client/js/app/controllers/NegociacaoController.js
+++ personal-projects/js-avancado1/client/js/app/controllers/NegociacaoController.js
@@ -5,25 +5,38 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
-
         event.preventDefault();
 
-        let data = new Date(
-            ...this._inputData.value.split('-').
-            map((item, indice) => item - indice % 2)
-        );
-        console.log(data);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
 
-        let negociacao = new Negociacao(
-            data,
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+
+        this._limpaFormulario();
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
-        console.log(negociacao);
+            this._inputValor.value);
     }
 
 }
