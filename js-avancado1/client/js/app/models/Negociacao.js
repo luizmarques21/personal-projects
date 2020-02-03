6837be34736860e44c52c1158adaf4c156ba1d05
Luiz Mariel Leite Marques <luizmariel@moobitech.com.br>
2020-02-03 08:24:31 -0300

    js avançado

--- js-avancado1/client/js/app/models/Negociacao.js
+++ js-avancado1/client/js/app/models/Negociacao.js
@@ -1,18 +1,18 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this.data = new Date(data.getTime());
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this);
     }
 
-    getVolume() {
+    get volume() {
         return this._quantidade * this._valor;
     }
 
-    getData() {
-        return this.data;
+    get data() {
+        return this._data;
     }
 
     get quantidade() {
