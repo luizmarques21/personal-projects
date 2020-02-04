ce096b86b761c590e729b61d06c296b9375aaaed
Luiz Mariel Leite Marques <luizmariel@moobitech.com.br>
2020-02-04 17:47:16 -0300

    Remoção de arquivos

--- roteamento/View/editar-empresa-post.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$oEmpresa = new Empresa($_REQUEST['cnpj'], $_REQUEST['razao_social'], $_REQUEST['nome_fantasia'],
-	$_REQUEST['email'], $_REQUEST['telefone']);
-$oEmpresa->editarEmpresa($_REQUEST['id']);
-header("refresh:1;url=../empresa");
