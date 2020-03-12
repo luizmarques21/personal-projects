9323b876bd024f3462e0b083a8e50683973a6c5f
Luiz Mariel Leite Marques <luizmariel@moobitech.com.br>
2020-03-12 17:58:00 -0300

    Projeto do curso de Laravel

---
+++ personal-projects/php-laravel/app/Services/CriadorDeSerie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use App\Serie;
+
+class CriadorDeSerie {
+
+    public function criarSerie($nomeSerie, $qtdTemporada, $epPorTemporada) {
+        $serie = Serie::create(['nome' => $nomeSerie]);
+        $qtdTemporadas = $qtdTemporada;
+        for ($i = 0; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+
+            for ($j = 1; $j <= $epPorTemporada; $j++) {
+                $temporada->episodios()->create(['numero' => $j]);
+            }
+        }
+
+        return $serie;
+    }
+
+}
