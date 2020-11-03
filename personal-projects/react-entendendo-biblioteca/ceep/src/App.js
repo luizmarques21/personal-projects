9e17431eba1cd5d1dd73c91bda345e5d52cc9172
Luiz Mariel Leite Marques <luizmariel@moobitech.com.br>
2020-11-02 23:12:08 -0300

    Arquivos do curso de React Ciclo de vida dos componentes

--- personal-projects/react-entendendo-biblioteca/ceep/src/App.js
+++ personal-projects/react-entendendo-biblioteca/ceep/src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from "react";
 import FormularioCadastro from "./componentes/FormularioCadastro/FormularioCadastro";
 import ListaDeNotas from "./componentes/ListaDeNotas/ListaDeNotas";
+import ListaDeCategorias from "./componentes/ListaDeCategorias";
 import "./assets/App.css";
 import "./assets/index.css";
+import Categorias from "./dados/Categorias.js";
+import ArrayDeNotas from "./dados/Notas.js";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      notas: [],
-    };
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
   }
-  criarNota(titulo, texto) {
-    const novaNota = { titulo, texto };
-    const novoArrayNotas = [...this.state.notas, novaNota];
-    const novoEstado = {
-      notas: novoArrayNotas,
-    };
-    this.setState(novoEstado);
-  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.state.notas} />
+        <FormularioCadastro
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
+          categorias={this.categorias}
+        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            categorias={this.categorias}
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+          />
+          <ListaDeNotas
+            notas={this.notas}
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+          />
+        </main>
       </section>
     );
   }
