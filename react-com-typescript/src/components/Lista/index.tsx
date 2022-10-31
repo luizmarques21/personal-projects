bea73cafc727c3cf7e2546006f2d54b0df91336c
Luiz Mariel Leite Marques <luizmariel@moobitech.com.br>
2022-10-31 20:38:17 -0300

    Aula 04 Curso React com TS

--- react-com-typescript/src/components/Lista/index.tsx
+++ react-com-typescript/src/components/Lista/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
@@ -12,10 +12,7 @@ function Lista() {
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
+                    <Item {...item} key={index} />
                 ))}
             </ul>
         </aside>
